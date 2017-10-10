46e615751ac28e47a7d33c2c28aa285fe2aa9c27
YulijaU <cenite-moment@mail.ru>
2017-10-10 20:04:39 +0700

    Version 1.1.0  Uses a new function with wider capacity that includes the old function inside itself
    Supports several parameters for underlining. Loops through an array of elements of the same kind

--- script.js
+++ script.js
@@ -1,46 +1,64 @@
-function splitBySimbols(str, simbols) {    
-    var splittedStr = [];
-    for (var i = 0; i < simbols.length; i += 1) {
-        splittedStr = str.split(simbols[i]);
-        str = splittedStr.join('!' + simbols[i]);
+// domEntity < String >: class / id / tag
+// domEntityValue < String > : value of attribute (for class or id) or tag name (for tag)
+function underlineElement(domEntity, domEntityValue, simbolsToEscape) {
+    var elementsToUnderline, 
+        textFromAnElement = '';
+    if (domEntity === 'class') {
+        elementsToUnderline = document.getElementsByClassName(domEntityValue);
+        applyToAllElements(elementsToUnderline, textFromAnElement, simbolsToEscape);
+    } else if (domEntity === 'tag') {
+        elementsToUnderline = document.getElementsByTagName(domEntityValue);
+        applyToAllElements(elementsToUnderline, textFromAnElement, simbolsToEscape);
+    } else if (domEntity === 'id') {
+        elementsToUnderline = document.getElementById(domEntityValue);
+        textFromAnElement = elementsToUnderline.innerText;
+        textFromAnElement = textFromAnElement.replace(/\r?\n/g, '<br/>');    
+        splitBySimbols(elementsToUnderline, textFromAnElement, simbolsToEscape);
+    } else {
+        console.log('This DOM entity is not supported!');
     }
-    splittedStr = str.split('!');
-    borderString(splittedStr, simbols);
+}
+
+function applyToAllElements(elementsToUnderline, textFromAnElement, simbolsToEscape) {
+    for (var i = 0; i < elementsToUnderline.length; i += 1) {
+        textFromAnElement = elementsToUnderline[i].innerText;        
+        textFromAnElement = textFromAnElement.replace(/\r?\n/g, '<br/>');  
+        splitBySimbols(elementsToUnderline[i], textFromAnElement, simbolsToEscape);
+    }
+}
+
+function splitBySimbols(element, textFromAnElement, simbolsToEscape) {    
+    var splittedStr = [];
+    for (var i = 0; i < simbolsToEscape.length; i += 1) {
+        splittedStr = textFromAnElement.split(simbolsToEscape[i]);
+        textFromAnElement = splittedStr.join('!' + simbolsToEscape[i] + '!');
+    }
+    splittedStr = textFromAnElement.split('!');
+    console.log(splittedStr);
+    addClassToUnderline(element, splittedStr, simbolsToEscape);
 }
 
 // Creates a border-bottom to an element with the string until a simbol
-function borderString(splittedStr, simbols) {        
+function addClassToUnderline(element, splittedStr, simbolsToEscape) {        
     for (var i = 0; i < splittedStr.length; i += 1) {        
-        if ((splittedStr[i] !== "g") && (splittedStr[i] !== "y")) {        
-        // var check = false;
-        // checkOnBorderNeed(splittedStr[i], simbols)
-        // if (check) {
+        // Create a loop for conditions
+        if ((splittedStr[i] !== "g") && (splittedStr[i] !== "y") && (splittedStr[i] !== "p")) {        
             splittedStr[i] = `<span class="bordered">` + splittedStr[i] + `</span>`;
         }
     }    
-    str = splittedStr.join('');    
-    document.getElementsByTagName('h1')[0].innerHTML = str;
+    textFromAnElement = splittedStr.join('');    
+    element.innerHTML = textFromAnElement;
 }
 
-// function checkOnBorderNeed(item, simbols) {    
-//     for (var i = 0; i < simbols.length; i += 1) {
-//         check = false;
-//         if (item !== simbols[i]) {
-//             check = true;
-//         } else {
-//             check = false;
-//             break;
-//         }
-//     }
-//     console.log(item);
-//     console.log(check);
-//     return check;
+// Try to pass object parameter to getElement function
+// var elementToUnderline = {
+//     domEntity: 'class', // <String> 'id', 'class', 'tag'
+//     domEntityValue: 'bordered', // <String>
+//     arrayPosition: 2, // < Int >
+//     simbolsToEscape: [' ', 'g', 'y', 'p'] // < Array >
 // }
 
 window.onload = function () {
-    var h1 = document.getElementsByTagName('h1')[0];
-    var textFromAnElement = h1.innerText;
-    textFromAnElement = textFromAnElement.replace(/\r?\n/g, '<br/>');
-    console.log(textFromAnElement);
-    splitBySimbols(textFromAnElement, [/*' ', */'<br/>', 'g', 'y']);
+    var simbolsToEscape = [' ', 'g', 'y', 'p'];
+    underlineElement('class', 'sentence', simbolsToEscape);
 };
